b0bbd44f16d571ef98f79731b91581924f1d8894
Anna Langner <annalangner06@gmail.com>
2020-03-06 15:32:12 +0100

    Convert to ES6 classes

--- app.js
+++ app.js
@@ -7,67 +7,66 @@ const bookIsbn = document.getElementById('exampleInputISBN');
 const tableBody = document.getElementById('table-body');
 const btnSubmit = document.getElementById('btn-submit');
 
-function Book(bookTitle, bookAuthor, bookIsbn) {
-  this.bookTitle = bookTitle;
-  this.bookAuthor = bookAuthor;
-  this.bookIsbn = bookIsbn;
+class Book {
+  constructor (bookTitle, bookAuthor, bookIsbn) {
+    this.bookTitle = bookTitle;
+    this.bookAuthor = bookAuthor;
+    this.bookIsbn = bookIsbn;
+  }    
 }
 
-function UI() {}
+class UI {
+  addBookToList(book) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${book.bookTitle}</td>
+    <td>${book.bookAuthor}</td>
+    <td>${book.bookIsbn}</td>
+    <td><a href="#" class="remove-btn text-danger">X</a></td>
+    `;
+    tableBody.appendChild(row);  
+  }
 
-UI.prototype.addBookToList = function(book) {
-  const row = document.createElement('tr');
-  row.innerHTML = `
-  <td>${book.bookTitle}</td>
-  <td>${book.bookAuthor}</td>
-  <td>${book.bookIsbn}</td>
-  <td><a href="#" class="remove-btn text-danger">X</a></td>
-  `;
-  tableBody.appendChild(row);
+  clearFields() {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookIsbn.value = '';
+  }
 
-}
-
-UI.prototype.clearFields = function() {
-  bookTitle.value = '';
-  bookAuthor.value = '';
-  bookIsbn.value = '';
-}
-
-UI.prototype.showAlert = function(alert, className) {
-  const divAlert = document.createElement('div');
-  divAlert.className = `alert ${className}`;
-  divAlert.appendChild(document.createTextNode(alert));
-
-  container.insertBefore(divAlert, form);
-
-  setTimeout(function() {
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
-
-UI.prototype.deleteBook = function(target) {
-  if(target.classList.contains("remove-btn")) {
-    target.parentElement.parentElement.remove();  
+  showAlert(alert, className) {
     const divAlert = document.createElement('div');
-    divAlert.className = 'alert alert-danger';
-    divAlert.appendChild(document.createTextNode('This item has been deleted'));
-
+    divAlert.className = `alert ${className}`;
+    divAlert.appendChild(document.createTextNode(alert));
+  
     container.insertBefore(divAlert, form);
-
+  
     setTimeout(function() {
       document.querySelector('.alert').remove();
-    }, 3000);  
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if(target.classList.contains("remove-btn")) {
+      target.parentElement.parentElement.remove();  
+      const divAlert = document.createElement('div');
+      divAlert.className = 'alert alert-danger';
+      divAlert.appendChild(document.createTextNode('This item has been deleted'));
+  
+      container.insertBefore(divAlert, form);
+  
+      setTimeout(function() {
+        document.querySelector('.alert').remove();
+      }, 3000);  
+    }
   }
 }
-
 
 form.addEventListener('submit', addBookItem);
 
 function addBookItem(e) {
   const title = bookTitle.value;
   const author = bookAuthor.value;
-  const isbn = bookIsbn.value;
-  
+  const isbn = bookIsbn.value;  
   const ui = new UI();
 
   if(title === '' || author === '' || isbn === '') {
@@ -78,9 +77,7 @@ function addBookItem(e) {
   const book = new Book (title, author, isbn);
 
   ui.addBookToList(book);
-
   ui.clearFields();
-
   ui.showAlert('This item has been added',  'alert-success'); 
 
    e.preventDefault();
@@ -89,5 +86,6 @@ function addBookItem(e) {
 tableBody.addEventListener('click', function(e) {  
   const ui = new UI;
   ui.deleteBook(e.target);
+  
   e.preventDefault();
 });
